3a64be673f74405c3a36a49228b32b9380ef99e5
Teddys09 <ted3009@hotmail.fr>
2022-05-03 15:23:26 +0200

    Ajout event listener sur la couleur et la quantité plus stockage local en json stringify

--- front/js/product.js
+++ front/js/product.js
@@ -3,7 +3,9 @@ const urlParams = new URLSearchParams(window.location.search);
 // On prend l'id de la page du kanap
 // On fait une demande get qui retourne la première valeur associé
 const id = urlParams.get('_id');
-console.log(id);
+// On définit les variables globale pour les utiliser plus tard
+let kanapPrice = 0;
+let imgUrl, altText;
 // On prend le contenu de l'API en format JSON
 fetch(`http://localhost:3000/api/products/${id}`)
   .then((res) => res.json())
@@ -13,6 +15,11 @@ fetch(`http://localhost:3000/api/products/${id}`)
     document.querySelector('#title').innerHTML = '<h1>erreur 404</h1>';
     console.log('erreur 404, sur ressource api:' + err);
   });
+// On fait appel à la fonction lesKanaps qui est toujours dans fecth
+/* On laisse la fonction dans fetch pour que si les infos demandé 
+     Mettent beaucoup de temps à arriver la function lesKanaps ne se 
+    déclenchera que quand get aura tout reçu*/
+// Puis on lui passe les directives
 function lesKanaps(kanap) {
   /* const altTxt = kanap.altTxt;
   const colors = kanap.colors;
@@ -26,13 +33,17 @@ function lesKanaps(kanap) {
   */
 
   const { altTxt, colors, description, imageUrl, name, price, id } = kanap;
+  // On appel les fonction make qui vont chercher et créer les éléments demandé
+  kanapPrice = price;
+  imgUrl = imageUrl;
+  altText = altTxt;
   makeImage(imageUrl, altTxt);
   makeTitle(name);
   makePrice(price);
   makeDescription(description);
   makeColors(colors);
 }
-
+// Créer un element image
 function makeImage(imageUrl, altTxt) {
   const image = document.createElement('img');
   image.src = imageUrl;
@@ -40,18 +51,19 @@ function makeImage(imageUrl, altTxt) {
   const parent = document.querySelector('.item__img');
   parent.appendChild(image);
 }
+// Créer un element titre
 function makeTitle(name) {
   document.querySelector('#title').textContent = name;
 }
-
+// Créer un element prix
 function makePrice(price) {
   document.querySelector('#price').textContent = price;
 }
-
+// Créer un element description
 function makeDescription(description) {
   document.querySelector('#description').textContent = description;
 }
-
+// Créer un element couleur + les choix d'options de couleurs
 function makeColors(colors) {
   const colorPlace = document.querySelector('#colors');
   colors.forEach((color) => {
@@ -61,3 +73,23 @@ function makeColors(colors) {
     colorPlace.appendChild(option);
   });
 }
+
+const button = document.querySelector('#addToCart');
+button.addEventListener('click', (e) => {
+  const color = document.querySelector('#colors').value;
+  const quantity = document.querySelector('#quantity').value;
+  if (color == null || color === '' || quantity == null || quantity == 0) {
+    alert('Selectionner une couleur et une quantité ');
+    return;
+  }
+  const storage = {
+    id: id,
+    color: color,
+    quantity: Number(quantity),
+    price: kanapPrice,
+    imageUrl: imgUrl,
+    altTxt: altText,
+  };
+  localStorage.setItem(id, JSON.stringify(storage));
+  window.location.href = 'cart.html';
+});
